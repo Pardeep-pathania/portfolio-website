534a5f24a96c6dd780be7f2cda9ef1536a9b804d
pardeep-pathania <pathanianikhil145@gmail.com>
2025-08-05 14:25:03 +0530

    experience section completed

--- src/components/experienceSection/AllExperience.jsx
+++ src/components/experienceSection/AllExperience.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { FaArrowRightLong } from "react-icons/fa6";
+import SingleExperience from './SingleExperience'
+
+const experiences = [
+  {
+    job: 'Python Developer Intern',
+    company: "Future Finders",
+    date: "July 2023 - August 2023",
+    responsibility:[
+       "Implementing reusable components.",
+      "Participating in large scale application.",
+      "Working on the performance of web applications.",
+      "Generating new ideas for better user experience.",
+    ]
+  },
+
+    {
+    job: 'MERN Stack Developer Intern',
+    company: "ThinkNext Technologies",
+    date: "Feb 2025 - July 2025 ",
+    responsibility:[
+       "Implementing reusable components.",
+      "Participating in large scale application.",
+      "Working on the performance of web applications.",
+      "Generating new ideas for better user experience.",
+    ]
+  }
+]
 
 const AllExperience = () => {
   return (
-    <div>
-      
+    <div className='flex md:flex-row sm:flex-col items-center justify-around'>
+      {
+        experiences.map((experience, index)=>{
+          return <>
+            <SingleExperience key={index} experience= {experience} />
+            {index<1 ? (<FaArrowRightLong className="text-6xl text-orange-500 lg:block sm:hidden"/>) : ("") }
+          </>
+        })
+      }
     </div>
   )
 }
--- src/components/experienceSection/SingleExperience.jsx
+++ src/components/experienceSection/SingleExperience.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const SingleExperience = () => {
+const SingleExperience = ({ experience }) => {
   return (
-    <div>
-      
+    <div className="md:h-[350px] md:w-[240px] sm:h-auto sm:w-auto border-2 border-orange-500 border-dashed rounded-2xl mt-12 p-4  ">
+      <p className="font-bold text-cyan-500">{experience.job}</p>
+      <p className="text-orange-500">{experience.company}</p>
+      <p className="text-gray-300">{experience.date}</p>
+      <ul className="list-disc mt-4 pl-4 text-white">
+        {experience.responsibility.map((resp, index) => {
+          return <li key={index}>{resp}</li>;
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SingleExperience
+export default SingleExperience;
