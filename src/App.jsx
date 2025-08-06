625aeece3dc09b5f60e1ae1751e5f082f09e1956
pardeep-pathania <pathanianikhil145@gmail.com>
2025-08-06 14:43:12 +0530

    contact section completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SkillsMain from './components/skillSection/SkillsMain'
 import SubSkills from './components/skillSection/SubSkills'
 import ExperienceMain from './components/experienceSection/ExperienceMain'
 import ProjectsMain from './components/projectSection/ProjectsMain'
+import ContactMeMain from './components/contactSection/ContactMeMain'
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
       <SubSkills/>
       <ExperienceMain/>
       <ProjectsMain/>
+      <ContactMeMain/>
 
       <HelperSection/>
     </main>
